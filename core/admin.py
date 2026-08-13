bd54f596374f6dd253e92145168df2c1a9f46994
Heng-Pok <hengsocheatjr@gmail.com>
2026-08-13 16:29:12 -0700

    Playing with Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Person
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "age")
+    list_filter = ("age",)
+    search_fields = ("first_name", "last_name")
--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
 
     def __str__(self):
         return str(self.first_name)
